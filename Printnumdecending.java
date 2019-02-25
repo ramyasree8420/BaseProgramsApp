cf83555daabaa17589b1dbb8849144c2d1d8dfd1
ramyasree.b8420 <ramyasree.b8420@gmail.com>
2019-02-25 02:14:07 -0800

    decending

---
+++ Printnumdecending.java
@@ -0,0 +1,11 @@
+package com.regnant;
+
+public class Printnumdecending {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int x=10;x>=0;x--)
+			System.out.println(x);
+	}
+
+}
