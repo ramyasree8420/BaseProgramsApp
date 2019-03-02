59a4c28c357c9e5df18d06c71f87d16a1de63777
ramyasree8420 <ramyasree.b8420@gmail.com>
2019-03-02 02:38:10 -0800

    oddnumers

---
+++ Oddnum.java
@@ -0,0 +1,16 @@
+package com.regnant;
+
+public class Oddnum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int x=1;x<=65;x++)
+		{
+			if(x%2!=0)
+			System.out.println(x);
+				
+		}
+
+	}
+
+}
