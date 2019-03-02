0bc0004067fc9dc7eb828335d18af9ee2a78b7a8
ramyasree8420 <ramyasree.b8420@gmail.com>
2019-03-02 02:33:38 -0800

    even numbers

---
+++ Evennum.java
@@ -0,0 +1,17 @@
+package com.regnant;
+
+public class Evennum {
+
+	public static void main(String[] args) {
+for(int x=1;x<=10;x++)
+{
+	if(x%2==0)
+	System.out.println(x);
+
+}
+		
+ 
+	}
+	
+		
+	}
